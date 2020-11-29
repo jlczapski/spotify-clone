073253694ded8b3c9c692afa040a869459bc9d00
Janusz Czapski <januszczapski@gmail.com>
2020-11-29 20:39:19 +0000

    Added Tailwind css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
+import '@/assets/css/tailwind.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render(h) { return h(App); },
+    render(h) { return h(App); },
 }).$mount('#app');
